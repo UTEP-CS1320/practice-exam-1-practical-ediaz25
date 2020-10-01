ad9d4456c46e9285e13a13bbcfc90c024a9986ed
ediaz25 <ediaz25@miners.utep.edu>
2020-10-01 23:40:36 +0000

    now working properly

--- p1_avg.h
+++ p1_avg.h
@@ -6,10 +6,17 @@ void main_avg(void) {
   if(n == 3) {
     int x1, x2, x3;
     scanf("%d %d %d", &x1, &x2, &x3);
-    printf("%d\n", (x1+x2+x3) / 3);
+    x2 = (double)x2;//convert every int into a double in this if statement
+    x1 = (double)x1;
+    x3 = (double)x3;
+    printf("%lf\n", (x1+x2+x3) / 3.00);
   } else {
     int x1, x2, x3, x4;
     scanf("%d %d %d %d", &x1, &x2, &x3, &x4);
-    printf("%d\n", (x1+x2+x3+x4) / 3);
+    x2 = (double)x2;//convert every int into a double in this else statement
+    x1 = (double)x1;
+    x3 = (double)x3;
+    x4 = (double)x4;
+    printf("%lf\n", (x1+x2+x3+x4) / 4.00);
   }
 }
